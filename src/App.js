f364f01e4488b438d2af58a73e5bf49b96319c94
claudioney <claudioney@gmail.com>
2018-11-18 11:46:02 -0200

    Adicionando react router para teste (#2)
    * Adicionando react router para teste
    
    * Adicionando react router link para voltar a pagina inicial

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, {Component} from 'react';
 
 import ChatRoom from './components/ChatRoom';
 
+import { Link } from 'react-router-dom'
+
 class App extends Component{
  render(){
+ 	document.title = 'Olá cordova'
     return(
 	  	<div>
-	  	 <ChatRoom/>
+	  	    <h3>Escolha uma das opções</h3>
+              <br/>
+              <Link to="/sala">Ir para a página de chat</Link>
+              <br/>
+              <Link to="/sobre">Ir para a página sobre</Link>
 		</div>
     )
  }
