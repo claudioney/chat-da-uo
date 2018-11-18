f2fefb513f26ee338d017e0f4626c92414e8c6c5
Claudioney <claudioney@gmail.com>
2018-11-18 11:39:38 -0200

    Adicionando react router link para voltar a pagina inicial

--- src/Sala.js
+++ src/Sala.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 
 import ChatRoom from './components/ChatRoom';
+import { Link } from 'react-router-dom'
+
 
 class Sala extends Component{
  render(){
     return(
 	  	<div>
 	  	 <ChatRoom/>
+	  	 <Link to="/">Pagina Inicial</Link>
 		</div>
     )
  }
--- src/Sobre.js
+++ src/Sobre.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom'
 
 class Sobre extends Component{
  render(){
     return(
 	  	<div>
-	  	 Sobre o dev que fez essa tela:<br/>
+	  	 Sobre o dev que fez essa tela:
+	  	 <br/>
 	  	 <h2>Claudioney Loureiro</h2>
+	  	 <br/>
+	  	 <Link to="/">Pagina Inicial</Link>
 		</div>
     )
  }
